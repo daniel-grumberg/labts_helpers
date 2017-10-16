71870b111d85ba956b0b8b7f448f703b03bb4cdd
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2017-10-16 09:54:07 +0100

    Add encryption to credentials using Fernet scheme

---
+++ encrypt_credentials.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+
+import argparse
+from cryptography.fernet import Fernet
+
+def decrypt(credentials_file, key_file):
+    credentials = b''
+    key = b''
+    with open(credentials_file, 'rb') as cf:
+        credentials = cf.read(-1)
+
+    with open(key_file, 'rb') as kf:
+        key = kf.read(-1)
+
+    f = Fernet(key)
+    byte_str = f.decrypt(credentials)
+    return byte_str.decode('utf-8')
+
+def main(args):
+    token_str = args.username + '\n' + args.password
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    token = f.encrypt(token_str.encode('utf-8'))
+    with open(args.output, 'w+b') as out_file:
+        out_file.write(token)
+
+    with open(args.key, 'w+b') as key_file:
+        key_file.write(key)
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    arg_parser.add_argument('username',
+            help='''Your DoC username''')
+    arg_parser.add_argument('password',
+            help='''Your DoC password''')
+    arg_parser.add_argument('-o', '--output',
+            default='./credentials.bin',
+            help='''The file where to store your encrypted credentials''')
+    arg_parser.add_argument('-k', '--key',
+            default='./key.bin',
+            help='''The file to store your secret key in''')
+    args = arg_parser.parse_args()
+    main(args)
+
--- labts_parsers.py
+++ labts_parsers.py
@@ -3,64 +3,64 @@
 from html.parser import HTMLParser
 
 class LabTSMainPageParser(HTMLParser):
-  def __init__(self):
-    self.table_nest = 0
-    self.links = {}
-    self.curr_td = False
-    self.curr_link = ''
-    super().__init__()
+    def __init__(self):
+        self.table_nest = 0
+        self.links = {}
+        self.curr_td = False
+        self.curr_link = ''
+        super().__init__()
 
-  def handle_starttag(self, tag, attrs):
-    if tag == 'table':
-      self.table_nest += 1
+    def handle_starttag(self, tag, attrs):
+        if tag == 'table':
+            self.table_nest += 1
 
-    if tag == 'td':
-      self.curr_td = True
+        if tag == 'td':
+            self.curr_td = True
 
-    if tag == 'a' and self.table_nest == 2:
-      attrs_dict = dict(attrs)
-      self.curr_link = attrs_dict['href']
+        if tag == 'a' and self.table_nest == 2:
+            attrs_dict = dict(attrs)
+            self.curr_link = attrs_dict['href']
 
-  def handle_data(self, data):
-    if self.curr_link != "":
-      self.links[data] = self.curr_link
-      self.curr_link = ""
+    def handle_data(self, data):
+        if self.curr_link != "":
+            self.links[data] = self.curr_link
+        self.curr_link = ""
 
 
-  def handle_endtag(self, tag):
-    if tag == 'table':
-      self.table_nest -= 1
-    elif tag == 'td':
-      self.curr_td = False
+    def handle_endtag(self, tag):
+        if tag == 'table':
+            self.table_nest -= 1
+        elif tag == 'td':
+            self.curr_td = False
 
 ################################################################################
 
 class ExercisePageParser(HTMLParser):
-  def __init__(self, students):
-    self.students = students
-    self.submissions = {}
-    self.tds = 0
-    self.curr_student = ''
-    self.curr_tag = ''
-    super().__init__()
+    def __init__(self, students):
+        self.students = students
+        self.submissions = {}
+        self.tds = 0
+        self.curr_student = ''
+        self.curr_tag = ''
+        super().__init__()
 
-  def handle_starttag(self, tag, attrs):
-    self.curr_tag = tag
-    if tag == 'td':
-      self.tds += 1
-    if tag == 'a' and self.tds == 5:
-      attrs_dict = dict(attrs)
-      if 'final' in attrs_dict['href']:
-        self.submissions[self.curr_student] = attrs_dict['href']
+    def handle_starttag(self, tag, attrs):
+        self.curr_tag = tag
+        if tag == 'td':
+            self.tds += 1
+        if tag == 'a' and self.tds == 5:
+            attrs_dict = dict(attrs)
+            if 'final' in attrs_dict['href']:
+                self.submissions[self.curr_student] = attrs_dict['href']
 
-  def handle_data(self, data):
-    if self.tds == 1 and self.curr_tag == 'a':
-      for s in self.students:
-        if s in data.strip() and data.strip():
-          self.curr_student = s
-          break
+    def handle_data(self, data):
+        if self.tds == 1 and self.curr_tag == 'a':
+            for s in self.students:
+                if s in data.strip() and data.strip():
+                    self.curr_student = s
+                    break
 
-  def handle_endtag(self, tag):
-    if tag == 'td' and self.tds == 6:
-      self.tds = 0
-      self.curr_student = ''
+    def handle_endtag(self, tag):
+        if tag == 'td' and self.tds == 6:
+            self.tds = 0
+            self.curr_student = ''
